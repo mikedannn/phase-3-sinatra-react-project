b859cf2124ea7ae3426e6e6cecbe36c09579d8d1
Michael <michael.daniels1013@gmail.com>
2022-11-04 18:59:09 -0500

    Created Trip schema and model

--- db/migrate/20221104235527_create_trips.rb
+++ db/migrate/20221104235527_create_trips.rb
@@ -1,4 +1,11 @@
 class CreateTrips < ActiveRecord::Migration[6.1]
   def change
+    create_table :trips do |t|
+      t.integer :user_id
+      t.integer :destination_id
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :trip_notes
+    end
   end
 end
