26a13be83b827fa373b56cc763c8007aa70c390b
Michael <michael.daniels1013@gmail.com>
2022-11-04 17:46:03 -0500

    Added user Model and Schema

--- db/migrate/20221104223458_create_users.rb
+++ db/migrate/20221104223458_create_users.rb
@@ -1,4 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.string :full_name
+      t.string :username
+      t.string :password
+      t.datetime :date_of_birth
+      t.timestamps
+    end
   end
 end
