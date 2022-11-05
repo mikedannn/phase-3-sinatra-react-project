5ab38817116ddb9f230bba2fec9e186c068a71d6
Michael <michael.daniels1013@gmail.com>
2022-11-04 20:15:00 -0500

    Seeded 8 random users using Faker gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
+require 'faker'
+
+
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+5.times do 
+
+    User.create(
+        full_name: Faker::Name.name,
+        username: Faker::Internet.user('username'),
+        password: Faker::Internet.password(min_length: 8, max_length: 12),
+        date_of_birth: Faker::Date.birthdate({ min: 18, max: 65, mode: 'age' })
+    )
+end
+
 
 puts "✅ Done seeding!"
+
