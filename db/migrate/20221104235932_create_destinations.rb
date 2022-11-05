7d5eb7c9645873276992dd08b64557b86a5acf58
Michael <michael.daniels1013@gmail.com>
2022-11-04 19:05:47 -0500

    Created Destination schema and model

--- db/migrate/20221104235932_create_destinations.rb
+++ db/migrate/20221104235932_create_destinations.rb
@@ -1,4 +1,9 @@
 class CreateDestinations < ActiveRecord::Migration[6.1]
   def change
+    create_table :destinations do |t|
+      t.string :city_name
+      t.string :country_name
+      t.string :image_url
+    end
   end
 end
