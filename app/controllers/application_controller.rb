6e756dece6d32e470871c0b0c02dfa675e71b2c2
Michael <michael.daniels1013@gmail.com>
2022-11-18 17:33:40 -0600

    Removed unnecessary code from controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ require 'pry'
 
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
 
   ## USER 
 
